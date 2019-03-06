b1aef692dcddac71104caaa2f934f04d7ba8879a
Celestin Hermez <celestinhermez@gmail.com>
2019-03-05 19:59:24 -0800

    Trying to find closest station
    Still bugs to fix around finding the closest station

--- DataCollection/WeatherData/get_weather_data.py
+++ DataCollection/WeatherData/get_weather_data.py
@@ -3,16 +3,48 @@ import pandas as pd
 from google.cloud import bigquery
 from google.oauth2 import service_account
 import pandas_gbq
+from scipy.spatial import distance
+import numpy as np
 
+# Get a dataframe of all the cities at hand, along with their latitude and longitude
+conn = sqlite3.connect('../cityDB.sqlite')
+cities = pd.read_sql_query("SELECT cid, city, state, lat, lng  FROM cities;", conn)
+
+# Get the coordinates for the cities
+cities['coordinates'] = zip(cities.lat, cities.lng)
+
+# Get a list of all the stations in the US from NOAA data publicly available on BigQuery
 credentials = service_account.Credentials.from_service_account_file('bigquery_key.json')
-query = (
-    "SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` "
-    'WHERE state = "TX" '
-    "LIMIT 100"
-)
+stations_query = ("""SELECT *
+FROM `bigquery-public-data.noaa_gsod.stations`
+WHERE country = 'US'""")
+stations = pandas_gbq.read_gbq(stations_query,
+                               project_id="dva-destination-recommender",
+                               credentials=credentials, dialect='standard')
 
-df = pandas_gbq.read_gbq(query, project_id="dva-destination-recommender", credentials=credentials, dialect='standard')
-print(df)
+# Get the coordinates for the stations
+stations['coordinates'] = np.array(zip(stations.lat, stations.lon))
+
+def find_closest_station(city_coord, stations):
+    """
+    INPUTS:
+    city_coord (tuple): a tuple with the coordinates for the city
+    stations (NumPy array): an array of coordinates for various stations
+
+    OUTPUT:
+    ids (int): the station ID for the closest station to the city, both usaf and wban
+    """
+    stations_coord = stations['coordinates'].values
+    print(stations.coordinates.iloc[0])
+    closest_index = distance.cdist([city_coord], stations_coord).argmin()
+    usaf = stations.iloc[closest_index, 0]
+    wban = stations.iloc[closest_index, 1]
+
+    ids = str(usaf) + '|' + str(wban)
+
+    return ids
+
+print(find_closest_station((34.78, -76.85), stations))
 
 client = bigquery.Client.from_service_account_json('bigquery_key.json')
 query = (
@@ -31,8 +63,6 @@ query = (
 #     assert row[0] == row.name == row["name"]
 #     print(row)
 
-# Get a dataframe of all the cities at hand, along with their latitude and longitude
-conn = sqlite3.connect('../cityDB.sqlite')
-cities = pd.read_sql_query("SELECT cid, city, state, lat, lng  FROM cities;", conn)
+
 
 # Limited to 1000 calls per day. With 4000 cities, last year, 1 call per week: 52 * 4000

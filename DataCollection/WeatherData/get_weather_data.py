0f8b08c48f693e52874a33d5fb8cc3a4bf3b1714
Celestin Hermez <celestinhermez@gmail.com>
2019-03-05 19:01:49 -0800

    Bigquery to Pandas
    Set up the big query to output to a Pandas dataframe

--- DataCollection/WeatherData/get_weather_data.py
+++ DataCollection/WeatherData/get_weather_data.py
@@ -1,6 +1,18 @@
 import sqlite3
 import pandas as pd
 from google.cloud import bigquery
+from google.oauth2 import service_account
+import pandas_gbq
+
+credentials = service_account.Credentials.from_service_account_file('bigquery_key.json')
+query = (
+    "SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` "
+    'WHERE state = "TX" '
+    "LIMIT 100"
+)
+
+df = pandas_gbq.read_gbq(query, project_id="dva-destination-recommender", credentials=credentials, dialect='standard')
+print(df)
 
 client = bigquery.Client.from_service_account_json('bigquery_key.json')
 query = (
@@ -8,16 +20,16 @@ query = (
     'WHERE state = "TX" '
     "LIMIT 100"
 )
-query_job = client.query(
-    query,
-    # Location must match that of the dataset(s) referenced in the query.
-    location="US",
-)  # API request - starts the query
-
-for row in query_job:  # API request - fetches results
-    # Row values can be accessed by field name or index
-    assert row[0] == row.name == row["name"]
-    print(row)
+# query_job = client.query(
+#     query,
+#     # Location must match that of the dataset(s) referenced in the query.
+#     location="US",
+# )  # API request - starts the query
+#
+# for row in query_job:  # API request - fetches results
+#     # Row values can be accessed by field name or index
+#     assert row[0] == row.name == row["name"]
+#     print(row)
 
 # Get a dataframe of all the cities at hand, along with their latitude and longitude
 conn = sqlite3.connect('../cityDB.sqlite')

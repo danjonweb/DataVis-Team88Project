71b4d728c443bd2680e4a50952d3c7149e6447be
Celestin Hermez <celestinhermez@gmail.com>
2019-03-05 18:51:45 -0800

    Example bigquery
    Set up the BigQuery API to pull weather data

--- DataCollection/WeatherData/get_weather_data.py
+++ DataCollection/WeatherData/get_weather_data.py
@@ -1,7 +1,26 @@
 import sqlite3
 import pandas as pd
+from google.cloud import bigquery
+
+client = bigquery.Client.from_service_account_json('bigquery_key.json')
+query = (
+    "SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` "
+    'WHERE state = "TX" '
+    "LIMIT 100"
+)
+query_job = client.query(
+    query,
+    # Location must match that of the dataset(s) referenced in the query.
+    location="US",
+)  # API request - starts the query
+
+for row in query_job:  # API request - fetches results
+    # Row values can be accessed by field name or index
+    assert row[0] == row.name == row["name"]
+    print(row)
 
 # Get a dataframe of all the cities at hand, along with their latitude and longitude
 conn = sqlite3.connect('../cityDB.sqlite')
 cities = pd.read_sql_query("SELECT cid, city, state, lat, lng  FROM cities;", conn)
 
+# Limited to 1000 calls per day. With 4000 cities, last year, 1 call per week: 52 * 4000

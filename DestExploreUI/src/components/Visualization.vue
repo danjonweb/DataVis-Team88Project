c190587348702bfab6eab6e84dce8263c0fc0746
SchultzC <cas03j@gmail.com>
2019-03-23 00:19:03 -0700

    made US map responsive

--- DestExploreUI/src/components/Visualization.vue
+++ DestExploreUI/src/components/Visualization.vue
@@ -3,7 +3,7 @@
     <svg class="fuller">
       <path fill="#888" :d="c()"></path>
       <path stroke="#44D8CB" fill="none" :d="s()"></path>
-      <circle r="4" :cx="x()" :cy="y()" fill='red'></circle>
+      <circle r="4" :cx="x()" :cy="y()" fill="red"></circle>
     </svg>
   </section>
 </template>
@@ -28,13 +28,31 @@ export default {
     };
   },
   beforeMount() {
-    var projection = d3.geoAlbersUsa();
-    this.path = d3.geoPath().projection(projection);
+    d3.select(window).on("resize", this.sizeChange);
+
+    this.projection = d3.geoAlbersUsa();
+
+    this.path = d3.geoPath().projection(this.projection);
     this.s = () => this.path(states);
     this.c = () => this.path(nation);
-    //37.7576793,-122.5076401
-    this.x = () => projection([-122.5076401, 37.7576793])[0];
-    this.y = () => projection([-122.5076401, 37.7576793])[1];
+    this.sizeChange()
+  },
+  methods: {
+    sizeChange() {
+      if (window.innerWidth > 700){
+        this.projection.scale(0.9*window.innerWidth).translate([window.innerWidth/2.8, window.innerHeight/2.2]);
+      }else{
+        this.projection.scale(1.3*window.innerWidth).translate([window.innerWidth/2, window.innerHeight/3.5]);
+      }
+      
+
+      this.path = d3.geoPath().projection(this.projection);
+      this.s = () => this.path(states);
+      this.c = () => this.path(nation);
+      //37.7576793,-122.5076401
+      this.x = () => this.projection([-122.5076401, 37.7576793])[0];
+      this.y = () => this.projection([-122.5076401, 37.7576793])[1];
+    }
   }
 };
 </script>

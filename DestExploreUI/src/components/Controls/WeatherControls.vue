afd82aa304a1f87600a4b8ebe324b9bcf94fa0ca
SchultzC <cas03j@gmail.com>
2019-03-24 00:41:51 -0700

    added temp controls

--- DestExploreUI/src/components/Controls/WeatherControls.vue
+++ DestExploreUI/src/components/Controls/WeatherControls.vue
@@ -9,7 +9,37 @@
       </div>
     </div>
     <div class="content-holder">
-      <button @click="clickbutton">click</button>
+      <b-input-group class="temp-box" size="sm">
+        <b-input-group-text slot="prepend" class="pre-tag">Ideal Temp (F)</b-input-group-text>
+
+        <b-input-group-prepend is-text>
+          <input
+            type="checkbox"
+            :checked="idealOn"
+            v-on:change="idealChange()"
+            aria-label="Checkbox for following text input"
+          >
+        </b-input-group-prepend>
+        <b-form-input
+          type="number"
+          :disabled="!idealOn"
+          :value="idealTemp"
+          v-on:change="idealTempValueChange($event)"
+          aria-label="Text input with checkbox"
+        />
+      </b-input-group>
+
+      <b-input-group class="temp-box" size="sm">
+        <b-input-group-text slot="prepend" class="pre-tag">+/- Tolerance (F)</b-input-group-text>
+
+        <b-form-input
+          type="number"
+          :disabled="!idealOn"
+          :value="tempTol"
+          v-on:change="tempTolValueChange($event)"
+          aria-label="Text input with checkbox"
+        />
+      </b-input-group>
     </div>
   </div>
 </template>
@@ -20,14 +50,41 @@ export default {
   name: "Team88WeatherControls",
   components: {},
   data: function() {
-    return {};
+    return {
+      idealOn: false,
+      tempTol: 15,
+      idealTemp: 72
+    };
+  },
+  computed: {
+    // a computed getter
+    minTemp: function() {
+      return this.idealTemp - this.tempTol;
+    },
+    maxTemp: function() {
+      return this.idealTemp + this.tempTol;
+    }
   },
   methods: {
     getImgUrl(pic) {
       return require(`../../assets/${pic}`);
     },
-    clickbutton() {
-      this.$store.dispatch("modCities");
+    idealChange() {
+      this.idealOn = !this.idealOn;
+      if (!this.idealOn){
+        console.log('Temp Preference Off')
+      }else{
+        console.log("Min",this.minTemp, "Max",this.maxTemp);
+      }
+      
+    },
+    tempTolValueChange(e) {
+      this.tempTol = parseInt(e);
+      console.log("Min",this.minTemp, "Max",this.maxTemp);
+    },
+    idealTempValueChange(e) {
+      this.idealTemp = parseInt(e);
+      console.log("Min",this.minTemp, "Max",this.maxTemp);
     }
   }
 };
@@ -35,4 +92,19 @@ export default {
 
 <style scoped lang='scss'>
 @import "../../assets/stylesheets/ControlsMenus.scss";
+.temp-box {
+  margin: 1vh;
+  margin-top: 2vh;
+  width: 96%;
+  font-size: 5px;
+}
+.pre-tag {
+  width: 50vw;
+}
+
+@media (orientation: landscape), (min-width: 769px) {
+  .pre-tag {
+    width: 14vw;
+  }
+}
 </style>

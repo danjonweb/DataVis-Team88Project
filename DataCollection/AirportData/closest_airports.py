58009d33b314d8eb5f893ab132d8e11b4985b1cc
Celestin Hermez <celestin.hermez@digitas.com>
2019-04-04 17:20:46 -0700

    Fix airport_info

--- DataCollection/AirportData/closest_airports.py
+++ DataCollection/AirportData/closest_airports.py
@@ -14,10 +14,6 @@ airport_info = pd.read_csv('airports_info.txt', names=['openflights_airport_id',
 # Create a tuple holding the latitude and longitude of the airport
 airport_info['coordinates'] = airport_info.apply(lambda x: (x.latitude, x.longitude), axis=1)
 
-# Save this to SQLite database
-airport_info[['name', 'city', 'country', 'iata_code','latitude', 'longitude']].to_sql('airport_info', 
-                                                                conn, if_exists='replace', index=False)
-
 # Read in city information
 cities_info = pd.read_sql_query("SELECT cid, city, state, lat, lng  FROM cities;", conn)
 cities_info['coordinates'] = cities_info.apply(lambda x: (x['lat'], x['lng']), axis=1)
@@ -28,6 +24,10 @@ airports_with_price = airport_price.src.unique().tolist()
 
 # Only keep airports with price information
 airport_info = airport_info.loc[airport_info.iata_code.isin(airports_with_price),]
+
+# Save this to SQLite database
+airport_info[['name', 'city', 'country', 'iata_code','latitude', 'longitude']].to_sql('airport_info',
+                                                                conn, if_exists='replace', index=False)
 
 # Create a dataframe of cities and their 3 closest airports, within a radius of 40 km
 city_airport = []

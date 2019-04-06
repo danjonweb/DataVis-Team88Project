7683d378b297a1d2c33759394a0740633dd315eb
Dan Weber <dan.j.weber@gmail.com>
2019-04-06 15:50:17 -0700

    restaurant and activity scrapers

---
+++ DataCollection/ActivitiesRestaurants/activity_scraper.py
@@ -0,0 +1,154 @@
+import urllib2
+from urllib2 import quote
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import re
+import time
+import os
+import csv
+import io
+import json
+import traceback
+import sqlite3
+
+# options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+# driver = webdriver.Chrome(chrome_options=options)
+#
+# base_url = 'https://www.tripadvisor.com'
+# top_activity_url = base_url + '/Attractions-%s'
+# top_restaurants_url = base_url + '/Restaurants-%s-%sActivities-%s.html'
+
+# Get city ids
+# with open('Top5000Population.csv', 'rb') as csvfile:
+#     cities = list(csv.reader(csvfile))
+# cities = [{"city": x[0].strip(), "state": x[1].strip(), "search":quote(x[0].strip() + " " + x[1].strip())} for x in cities]
+# if os.path.exists("cityIds.txt"):
+#   os.remove("cityIds.txt")
+# for city in cities:
+#     search_url = 'https://www.tripadvisor.com/Search?q=' + city["search"]
+#     driver.get(search_url)
+#     driver.implicitly_wait(10)
+#
+#     try:
+#         resultDiv = driver.find_element_by_css_selector('.result')
+#         html = driver.execute_script("return arguments[0].innerHTML;", resultDiv)
+#
+#         x = re.search('\'\/Home-(g.*?)\'', html)
+#         if x and not(x.group(1) is None):
+#             city_id = x.group(1)
+#             with open("cityIds.txt", "a") as myfile:
+#                 myfile.write(city["city"].encode("utf-8") + "|" + city["state"] + "|" + city_id + "\n")
+#     except:
+#         print("Error: '" + search_url + "'")
+
+# conn = sqlite3.connect('cityDB.sqlite')
+# cur = conn.cursor()
+#
+# cur.execute('CREATE TABLE IF NOT EXISTS cities (cid TEXT, city TEXT, state TEXT, json TEXT)')
+# conn.commit()
+#
+# with open('cityIds.txt', 'r') as f:
+#     city_ids = f.read().splitlines()
+# for i, city in enumerate(city_ids):
+#     [city, state, cid] = city.split('|')
+#
+#     print 'Fetching ' + str(i+1) + ' of ' + str(len(city_ids)) + '. ' + cid + ' - ' + city + ', ' + state
+#
+#     activity_url = top_activity_url % cid
+#
+#     driver.get(activity_url)
+#     driver.implicitly_wait(10)
+#
+#     try:
+#         isCarouselItem = True
+#         elements = driver.find_elements_by_css_selector('.attractions-carousel-shelf-ShelfCarouselItem__text--2MxS5')
+#         if len(elements) == 0:
+#             elements = driver.find_elements_by_css_selector('.filter_list_0 a.taLnk')
+#             isCarouselItem = False
+#
+#         categories = {}
+#         for e in elements:
+#             cat_url = ""
+#             if isCarouselItem:
+#                 parent = e.find_element_by_xpath('./../../..')
+#                 cat_url = parent.get_attribute('href')
+#             else:
+#                 cat_url = e.get_attribute('href')
+#             x = re.search('-c.*-(?:t-.*?-)?', cat_url)
+#             # if the url doesn't match the -c and -t pattern, it is promotional
+#             if not(x is None):
+#                 cat_string = e.get_attribute('innerText')
+#                 y = re.search('(.*?) \((\d+)\)', cat_string)
+#                 cat_name = str(y.group(1))
+#                 cat_number = str(y.group(2))
+#                 cats = x.group(0).split('-')
+#                 cat1 = str(cats[1])
+#                 if not(cat1 in categories):
+#                     categories[cat1] = {}
+#                 if len(cats) > 3:
+#                     cat2 = str(cats[2])
+#                     categories[cat1][cat2] = {
+#                         "name": cat_name,
+#                         "number": cat_number
+#                     }
+#                 else:
+#                     categories[cat1]["name"] = cat_name
+#                     categories[cat1]["number"] = cat_number
+#
+#         result = json.dumps(categories)
+#         cur.execute('INSERT INTO cities (cid, city, state, json) values (?, ?, ?, ?)', (cid, city, state, result))
+#         conn.commit()
+#     except:
+#         print "error: " + city
+#         print '>>> traceback <<<'
+#         traceback.print_exc()
+#         print '>>> end of traceback <<<'
+#
+# conn.close()
+# driver.quit()
+
+cats = []
+
+conn = sqlite3.connect('cityDB.sqlite')
+cur = conn.cursor()
+cur.execute('SELECT * FROM cities') 
+for row in cur:
+    data = json.loads(row[3])
+    for key in data:
+        if key.startswith('c') and key not in cats:
+            cats.append(key)
+for cat in cats:
+    try:
+        cur.execute('ALTER TABLE cities ADD COLUMN ' + cat + ' integer')
+    except:
+        pass
+
+try:
+    cur.execute('ALTER TABLE cities ADD COLUMN total integer')
+except:
+    pass
+   
+cur2 = conn.cursor()     
+cur2.execute('SELECT * FROM cities')
+
+for row in cur2:
+    print row[0]
+    cid = row[0]
+    data = json.loads(row[3])
+    total = 0
+    print data
+    for cat in cats:
+        print cat
+        if cat in data:
+            count = int(data[cat]['number'])
+            total += count
+            conn.cursor().execute('UPDATE cities SET ' + cat + ' = ' + str(count) + ' WHERE cid = "' + cid + '"')
+        else:
+            conn.cursor().execute('UPDATE cities SET ' + cat + ' = 0 WHERE cid = "' + cid + '"')
+    conn.cursor().execute('UPDATE cities SET total = ' + str(total) + ' WHERE cid = "' + cid + '"')
+    
+conn.commit()
+conn.close()
+
---
+++ DataCollection/ActivitiesRestaurants/restaurant_scraper.py
@@ -0,0 +1,96 @@
+import urllib2
+from urllib2 import quote
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import re
+import time
+import os
+import csv
+import io
+import json
+import traceback
+import sqlite3
+import sys
+import math
+
+
+conn = sqlite3.connect('cityDB.sqlite')
+conn.text_factory = lambda x: unicode(x, 'utf-8', 'ignore')
+
+try:
+    conn.cursor().execute('ALTER TABLE cities ADD COLUMN restaurants TEXT')
+except:
+    pass
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('--blink-settings=imagesEnabled=false')
+driver = webdriver.Chrome(chrome_options=options)
+driver.implicitly_wait(10)
+
+base_url = 'https://www.tripadvisor.com/RestaurantSearch?geo=%s&zfp=10613&itags=10591&sortOrder=relevance&geobroaden=false'
+
+result = conn.cursor().execute('SELECT * FROM cities')
+
+count = 0
+for row in result:
+    cid = row[0]
+    
+    url = base_url % cid
+    try:
+        restaurants = {
+            "prices":{},
+            "cuisines":{}
+        }
+        
+        # Get the number of pages
+        pages = 0
+        driver.get(url)
+        index_str = driver.find_element_by_css_selector('.popIndex').get_attribute('innerText')
+        x = re.search('of (\d+)', index_str)
+        if x is not None and x.group(1) is not None:
+            total = int(x.group(1))
+            pages = int(math.ceil(total/30.0))
+        
+        page = 0
+        for _ in range(pages):
+            print '%s - %d - %d' % (cid, page, pages)
+            driver.get('%s&o=a%s' % (url, page))
+            page += 30
+            
+            try:
+                cuisine_items = driver.find_elements_by_css_selector('.listing[data-index] a.cuisine')
+                for item in cuisine_items:
+                    cuisine = item.get_attribute('innerText')
+                    if cuisine in restaurants["cuisines"]:
+                        restaurants["cuisines"][cuisine] += 1
+                    else:
+                        restaurants["cuisines"][cuisine] = 1
+            
+                price_items = driver.find_elements_by_css_selector('.listing[data-index] span.price')
+                for item in price_items:
+                    price = item.get_attribute('innerText')
+                    if price in restaurants["prices"]:
+                        restaurants["prices"][price] += 1
+                    else:
+                        restaurants["prices"][price] = 1
+            except:
+                print 'error on ' + ('%s&o=a%s' % (url, page))
+                continue
+            
+        
+        result = json.dumps(restaurants) 
+        print result
+        print ''
+        conn.cursor().execute('UPDATE cities SET restaurants = ? WHERE cid = ?', (result, cid))
+        conn.commit() 
+    except:
+        print "Error: " + cid
+        print url
+        traceback.print_exc()
+    
+    
+driver.quit()
+conn.close()
+

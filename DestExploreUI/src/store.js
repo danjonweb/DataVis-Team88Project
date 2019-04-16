0d790a3c0e5f89d4615a4ebb0713d970f8d57573
debmpaul <44852822+debmpaul@users.noreply.github.com>
2019-04-16 10:06:09 -0700

    Update store.js

--- DestExploreUI/src/store.js
+++ DestExploreUI/src/store.js
@@ -94,12 +94,9 @@ export default new Vuex.Store({
       },
       {
         name: 'Paul, Deborah',
-        info: 'Lorem ipsum dolor sit amet, \
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut \
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum \
-        dolore eu fugiat nulla pariatur.'
+        info: 'Edison Engineering Development Program at Baker Hughes GE \
+        Currently pursuing the Computational Data Analytics Track of the OMSA degree. \
+        Enjoying learning how to visualize data to form valuable insights.'
       },
       {
         name: 'Schultz, Chris',

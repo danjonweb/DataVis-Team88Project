61f7f8c92c526ce65d03b88ad87e7e5112cc5034
Yaoyao Fu <yaoyao.fu923@gmail.com>
2019-04-16 13:17:52 -0700

    add yaoyao info

--- DestExploreUI/src/store.js
+++ DestExploreUI/src/store.js
@@ -80,12 +80,8 @@ export default new Vuex.Store({
     members: [
       {
         name: 'Fu, Yaoyao',
-        info: 'Lorem ipsum dolor sit amet, \
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut \
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum \
-        dolore eu fugiat nulla pariatur.'
+        info: 'Scientist in onco-immunology and self-learner of programming language and algorithm \
+        first semester in the OMSCS program.'
       },
       {
         name: 'Hermez, Celestin',

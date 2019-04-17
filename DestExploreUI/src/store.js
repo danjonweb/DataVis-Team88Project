4d56b64c407eba83eabd0778752fffe0a0a41a96
Dan Weber <dan.j.weber@gmail.com>
2019-04-17 10:51:08 -0700

    Update store.js

--- DestExploreUI/src/store.js
+++ DestExploreUI/src/store.js
@@ -103,12 +103,8 @@ export default new Vuex.Store({
       },
       {
         name: 'Weber, Dan',
-        info: 'Lorem ipsum dolor sit amet, \
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut \
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum \
-        dolore eu fugiat nulla pariatur.'
+        info: 'Computer science and math teacher. This is my 6th class in OMSCS. Interested in crafting \
+        beautiful and intuitive interfaces in front of complex backends.'
       }
 
     ],
